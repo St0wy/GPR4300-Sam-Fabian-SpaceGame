1b07480eb8755435b855f1acac88059a9ff51d74
Fabian Huber <stowy@protonmail.ch>
2022-02-11 17:18:24 +0100

    added health system

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// Script to put on gameobjects that must have health like the player or enemies.
+/// </summary>
+public class Health : MonoBehaviour
+{
+	[SerializeField] private int maxHealthPoints = 15;
+	[SerializeField] private float destoryTime = 0f;
+	[SerializeField] private bool destroyWhenKilled = true;
+	
+	public delegate void HurtCallback(int healthPoint);
+	
+	public HurtCallback OnHurt { get; set; }
+	public bool IsAlive { get; private set; }
+	public int HealthPoints { get; private set; }
+
+	private void Awake()
+	{
+		HealthPoints = maxHealthPoints;
+	}
+
+	/// <summary>
+	/// Reduces the health of the gameobject and kills it if health = 0.
+	/// </summary>
+	/// <param name="ammount">The ammount of health to substract. Defaults to 1.</param>
+	public void ReduceHealth(int ammount = 1)
+	{
+		HealthPoints -= ammount;
+
+		if (HealthPoints <= 0)
+		{
+			IsAlive = false;
+		}
+
+		OnHurt?.Invoke(HealthPoints);
+
+		if (!IsAlive && destroyWhenKilled)
+		{
+			Destroy(gameObject, destoryTime);
+		}
+	}
+}
---
+++ Assets/Scripts/UI/HealthUI.cs
@@ -0,0 +1,21 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+	public class HealthUI : MonoBehaviour
+	{
+		[SerializeField] private Health healthComponent;
+		private TextMeshProUGUI text;
+
+		private void Awake()
+		{
+			text = GetComponent<TextMeshProUGUI>();
+		}
+
+		private void Update()
+		{
+			text.text = $"Life : {healthComponent.HealthPoints}";
+		}
+	}
+}
