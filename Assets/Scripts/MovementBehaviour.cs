adb2537d554f682e7e90ed672702293230d18bdb
Fabian Huber <stowy@protonmail.ch>
2022-02-11 17:31:01 +0100

    removed physics from movement

--- Assets/Scripts/MovementBehaviour.cs
+++ Assets/Scripts/MovementBehaviour.cs
@@ -1,24 +1,23 @@
 ﻿using System;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
 public class MovementBehaviour : MonoBehaviour
 {
 	[SerializeField] private float speed = 1f;
 
 	private IInputHandler inputHandler;
-	private Rigidbody2D rb;
 
 	private void Awake()
 	{
-		rb = GetComponent<Rigidbody2D>();
 		inputHandler = GetComponent<IInputHandler>();
 		inputHandler.Pause += Pause;
 	}
 
 	private void FixedUpdate()
 	{
-		rb.velocity = inputHandler.InputVector * speed * Time.deltaTime;
+		Vector3 movement = inputHandler.InputVector * speed * Time.deltaTime;
+
+		transform.position += movement;
 	}
 
 	private void Pause()
