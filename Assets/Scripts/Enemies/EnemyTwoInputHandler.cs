904cfb313186820f804588d1489c83c3aad7b965
SStyles93 <samuel.styles93@gmail.com>
2022-02-15 19:22:36 +0100

    EnemyManager

--- Assets/Scripts/Enemies/EnemyTwoInputHandler.cs
+++ Assets/Scripts/Enemies/EnemyTwoInputHandler.cs
@@ -4,95 +4,100 @@ using Random = UnityEngine.Random;
 
 namespace SpaceGame.Enemies
 {
-	public class EnemyTwoInputHandler : MonoBehaviour, IInputHandler
-	{
-		public event IInputHandler.InputEventHandler FirePrimary;
-		public event IInputHandler.InputEventHandler FireSecondary;
-		public event IInputHandler.InputEventHandler Pause;
+    public class EnemyTwoInputHandler : MonoBehaviour, IInputHandler
+    {
+        public event IInputHandler.InputEventHandler FirePrimary;
+        public event IInputHandler.InputEventHandler FireSecondary;
+        public event IInputHandler.InputEventHandler Pause;
 
-		[SerializeField] private Transform[] lines;
+        [SerializeField] private Transform[] lines;
 
-		[Attributes.ReadOnly]
-		[SerializeField]
-		private int chosenLineIndex;
+        [Attributes.ReadOnly]
+        [SerializeField]
+        private int chosenLineIndex;
 
-		[Tooltip("The horizontal size of the area where the enemy can be.")]
-		[SerializeField]
-		private RangedFloat horizontalLimit = new RangedFloat(-8f, 8f);
+        [Tooltip("The horizontal size of the area where the enemy can be.")]
+        [SerializeField]
+        private RangedFloat horizontalLimit = new RangedFloat(-8f, 8f);
 
-		[Tooltip("The time that the enemy spends moving between two shots.")]
-		[SerializeField]
-		private RangedFloat timeBetweenShoot = new RangedFloat(0.5f, 1.5f);
+        [Tooltip("The time that the enemy spends moving between two shots.")]
+        [SerializeField]
+        private RangedFloat timeBetweenShoot = new RangedFloat(0.5f, 1.5f);
 
-		private EnemyTwoState state;
-		private float shootingTimer;
+        private EnemyTwoState state;
+        private float shootingTimer;
 
-		public Vector2 InputVector { get; private set; }
-		private float NextShootTime => Random.Range(timeBetweenShoot.Min, timeBetweenShoot.Max);
+        public Vector2 InputVector { get; private set; }
+        private float NextShootTime => Random.Range(timeBetweenShoot.Min, timeBetweenShoot.Max);
 
-		private void Awake()
-		{
-			InputVector = new Vector2(0, -1);
-			state = EnemyTwoState.Falling;
-			chosenLineIndex = Random.Range(0, lines.Length);
-			shootingTimer = NextShootTime;
-		}
+        public Transform[] Lines { get => lines; set => lines = value; }
 
-		private void Update()
-		{
-			switch (state)
-			{
-				default:
-				case EnemyTwoState.Falling:
-					HandleFallingState();
-					break;
-				case EnemyTwoState.ShootingRight:
-					if (transform.position.x >= horizontalLimit.Max)
-					{
-						state = EnemyTwoState.ShootingLeft;
-						InputVector = new Vector2(-1, 0);
-					}
+        private void Awake()
+        {
+            InputVector = new Vector2(0, -1);
+            state = EnemyTwoState.Falling;
+            shootingTimer = NextShootTime;
+        }
+        private void Start()
+        {
+            chosenLineIndex = Random.Range(0, Lines.Length - 1);
+        }
+        private void Update()
+        {
+            switch (state)
+            {
+                default:
+                case EnemyTwoState.Falling:
+                    HandleFallingState();
+                    break;
+                case EnemyTwoState.ShootingRight:
+                    if (transform.position.x >= horizontalLimit.Max)
+                    {
+                        state = EnemyTwoState.ShootingLeft;
+                        InputVector = new Vector2(-1, 0);
+                    }
 
-					HandleShoot();
-					break;
-				case EnemyTwoState.ShootingLeft:
-					if (transform.position.x <= horizontalLimit.Min)
-					{
-						state = EnemyTwoState.ShootingRight;
-						InputVector = new Vector2(1, 0);
-					}
+                    HandleShoot();
+                    break;
+                case EnemyTwoState.ShootingLeft:
+                    if (transform.position.x <= horizontalLimit.Min)
+                    {
+                        state = EnemyTwoState.ShootingRight;
+                        InputVector = new Vector2(1, 0);
+                    }
 
-					HandleShoot();
-					break;
-			}
-		}
+                    HandleShoot();
+                    break;
+            }
+        }
 
-		private void HandleShoot()
-		{
-			shootingTimer -= Time.deltaTime;
+        private void HandleShoot()
+        {
+            shootingTimer -= Time.deltaTime;
 
-			if (!(shootingTimer <= 0f)) return;
-			
-			FirePrimary?.Invoke();
-			shootingTimer += NextShootTime;
-		}
+            if (!(shootingTimer <= 0f)) return;
 
-		private void OnDrawGizmos()
-		{
-			foreach (Transform line in lines)
-			{
-				float y = line.position.y;
-				Gizmos.DrawLine(new Vector3(-10, y), new Vector3(10, y));
-			}
-		}
+            FirePrimary?.Invoke();
+            shootingTimer += NextShootTime;
+        }
 
-		private void HandleFallingState()
-		{
-			// Check if we hit the chosen line
-			if (!(transform.position.y <= lines[chosenLineIndex].position.y)) return;
+        private void OnDrawGizmos()
+        {
+            foreach (Transform line in Lines)
+            {
+                float y = line.position.y;
+                Gizmos.DrawLine(new Vector3(-10, y), new Vector3(10, y));
+            }
+        }
 
-			state = EnemyTwoState.ShootingRight;
-			InputVector = new Vector2(1, 0);
-		}
-	}
+        private void HandleFallingState()
+        {
+            // Check if we hit the chosen line
+            if (lines.Length < 1) return;
+            if (!(transform.position.y <= Lines[chosenLineIndex].position.y)) return;
+
+            state = EnemyTwoState.ShootingRight;
+            InputVector = new Vector2(1, 0);
+        }
+    }
 }
