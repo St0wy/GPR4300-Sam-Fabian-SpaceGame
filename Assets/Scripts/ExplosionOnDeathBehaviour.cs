c8a7bebf6c8d2567c724aba0d7b5581676b0fb4f
Fabian Huber <stowy@protonmail.ch>
2022-02-16 11:15:58 +0100

    added explosion sound

---
+++ Assets/Scripts/ExplosionOnDeathBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using SpaceGame.ScriptableObjects;
+using UnityEngine;
+
+namespace SpaceGame
+{
+	[RequireComponent(typeof(Health))]
+	[RequireComponent(typeof(ParticleSystem))]
+	public class ExplosionOnDeathBehaviour : MonoBehaviour
+	{
+		[SerializeField] private SoundEffectScritableObject explosionSound;
+		[SerializeField] private ParticleSystem particles;
+		private Health health;
+
+		private void Awake()
+		{
+			particles = GetComponent<ParticleSystem>();
+			health = GetComponent<Health>();
+			health.OnHurt += (healthPoints) =>
+			{
+				if (healthPoints <= 0)
+				{
+					OnDeath();
+				}
+			};
+		}
+
+		private void OnDeath()
+		{
+			explosionSound.Play();
+			particles.Play();
+		}
+	}
+}
