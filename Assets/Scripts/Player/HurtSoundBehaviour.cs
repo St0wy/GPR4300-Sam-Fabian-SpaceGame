0ba1de47e828a3709747ac55b7ba1a8e95941302
Fabian Huber <stowy@protonmail.ch>
2022-02-16 13:34:40 +0100

    added hurt shound

---
+++ Assets/Scripts/Player/HurtSoundBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SpaceGame.ScriptableObjects;
+using UnityEngine;
+
+namespace SpaceGame.Player
+{
+	[RequireComponent(typeof(Health))]
+	public class HurtSoundBehaviour : MonoBehaviour
+	{
+		[SerializeField] private SoundEffectScritableObject hurtSound;
+
+		private Health health;
+
+		private void Awake()
+		{
+			health = GetComponent<Health>();
+			health.OnHurt += healthPoints => hurtSound.Play();
+		}
+	}
+}
